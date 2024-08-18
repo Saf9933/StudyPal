8948333c1a637d76c907d2d3ad761576d079a2f7
salma <salmafilali2.0@icloud.com>
2024-08-18 01:42:12 +0100

    Initial Commit

---
+++ StudyPal/StudyPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyPalApp.swift
+//  StudyPal
+//
+//  Created by salma filali on 2024-08-18.
+//
+
+import SwiftUI
+
+@main
+struct StudyPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
